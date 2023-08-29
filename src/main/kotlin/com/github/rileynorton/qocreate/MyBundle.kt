2ecb0340365cbc12d142cf12c55f6d9e018c40ef
GitHub Action <action@github.com>
2023-08-29 21:03:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/rileynorton/qocreate/MyBundle.kt
+++ src/main/kotlin/com/github/rileynorton/qocreate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.rileynorton.qocreate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/rileynorton/qocreate/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/rileynorton/qocreate/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.rileynorton.qocreate.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.rileynorton.qocreate.MyBundle
+import com.github.rileynorton.qocreate.services.MyProjectService
 import javax.swing.JButton
 
 
